f157fdfac5d19a4d83f7cc556a53be2560deb3cc
danielg <danieldanyg@gmail.com>
2024-07-19 09:53:55 -0500

    set up routing and finished home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import slothFace from "../../assets/sloth.webp";
+import slothFaceEyes from "../../assets/sloth-eyes.png";
+import { Link } from "react-router-dom";
+
+type Props = {};
+
+export default function index({}: Props) {
+  const [title, setTitle] = useState("");
+  const [isHovered, setIsHovered] = useState(false);
+
+  let i = 0;
+  function typeWriter() {
+    let txt = "Click the sloth!";
+    let speed = 200;
+    if (i < txt.length) {
+      setTitle(txt.slice(0, i + 1));
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+  useEffect(() => {
+    typeWriter();
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center items-center gap-20 text-center w-full h-screen">
+      <h1 className="typewriter text-6xl font-bold font-mono text-yellow-400">
+        {title}
+      </h1>
+      <Link to="/login">
+        <button onClick={typeWriter} className="w-80 rounded-full">
+          <img
+            src={isHovered ? slothFaceEyes : slothFace}
+            alt="sloth face"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          />
+        </button>
+      </Link>
+    </div>
+  );
+}
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+import LoginCard from './LoginCard'
+
+type Props = {}
+
+export default function index({}: Props) {
+  return (
+    <div className='flex justify-center items-center w-full h-screen'>
+        <LoginCard/>
+    </div>
+  )
+}
