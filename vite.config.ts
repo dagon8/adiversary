7f2fc1925a1a132b57279ea4df79d149f81f7ab9
dagon8 <danieldanyg@gmail.com>
2024-07-30 12:00:05 -0500

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: "/adiversary/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
